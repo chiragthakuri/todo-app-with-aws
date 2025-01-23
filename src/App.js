bbba6f3139a855d07b2b3d89bf65c98827b48664
Chirag <chiragbikram@gmail.com>
2025-01-23 15:52:51 -0800

    getTodo, getTodos, changeTodo, deleteTodo added

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import {Amplify} from 'aws-amplify';
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-import { createTodo } from './graphql/mutations';
+import { createTodo, updateTodo, deleteTodo } from './graphql/mutations';
 import { generateClient } from 'aws-amplify/api';
-import { listTodos } from './graphql/queries';
+import { listTodos, getTodo } from './graphql/queries';
 
 import awsExports from './aws-exports';
 Amplify.configure(awsExports);
@@ -12,8 +12,7 @@ Amplify.configure(awsExports);
 const client = generateClient();
 
 async function storeTodo(){
-  
-  const result = await client.graphql({
+    const result = await client.graphql({
     query: createTodo,
     variables: {
       input:{
@@ -23,12 +22,44 @@ async function storeTodo(){
   });
 }
 
+async function changeTodo(){
+  const result = await client.graphql({
+  query: updateTodo,
+  variables: {
+    input:{
+      id: "e6356257-a468-4dcb-9191-04b81882044b",
+      name: "wash doors updated"
+    }
+  }
+});
+}
+
 async function fetchTodos(){
   const result = await client.graphql({
     query: listTodos
   });
   console.log(result);
 }
+
+async function fetchTodo(){
+  const result = await client.graphql({
+    query: getTodo,
+    variables: { id: "e6356257-a468-4dcb-9191-04b81882044b"}
+  });
+  console.log(result);
+}
+
+async function removeTodo(){
+  const result = await client.graphql({
+  query: deleteTodo,
+  variables: {
+    input:{
+      id: "e6356257-a468-4dcb-9191-04b81882044b"
+    }
+  }
+});
+}
+
 
 const App = () => {
   return (
@@ -40,7 +71,13 @@ const App = () => {
 
           <button onClick={storeTodo}>New Todo</button>
 
-          <button onClick={FetchTodo}>Fetch Todos</button>
+          <button onClick={fetchTodos}>Fetch Todos</button>
+
+          <button onClick={fetchTodo}>Fetch Todo</button>
+
+          <button onClick={changeTodo}>Change Todo</button>
+
+          <button onClick={removeTodo}>Delete Todo</button>
         </main>
 
         
